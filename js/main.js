d8fb736682fddeead8263f2e47502930821b774f
JePellet <43410916+JePellet@users.noreply.github.com>
2018-10-24 18:33:14 +0200

    a

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// ripple sur le bouton
+// on scroll de 100vh dès qu'on scroll un tout petit peu
+// preventDefault() coment ça se comporte sur plusioeurs éléments
+// math.abs
+
+
+var page = document.querySelector('body');
+var bouton = document.querySelector('.button');
+
+bouton.addEventListener('click', function() {
+
+
+
+
+});
